e396baa9c3dee46a6f6ab854dd7c294e151d14cb
Justinas Simas <simjustinas@gmail.com>
2023-06-08 17:19:45 +0100

    changed cookie removal logic

--- src/stores/user.js
+++ src/stores/user.js
@@ -13,7 +13,6 @@ export const useUserStore = defineStore('user', {
 
   actions: {
     logout() {
-      document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
       VueCookies.remove('jwt')
 
       Object.assign(this, defaultState)
