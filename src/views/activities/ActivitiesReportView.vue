1266919e5eba4b4023cd0ee1eb0bd7554477982c
Justinas Simas <simjustinas@gmail.com>
2023-06-18 21:38:06 +0100

    final report preparatiot: done

--- src/views/activities/ActivitiesReportView.vue
+++ src/views/activities/ActivitiesReportView.vue
@@ -1,28 +1,61 @@
 <template>
-  <div class="container mt-4">
-    <h1>Image Marking</h1>
+  <div class="container mt-4 mb-5">
+    <router-link
+      class="align-self-start"
+      :to="{ name: 'schoolActivities', params: { schoolId: userStore.user.schoolId } }"
+    >
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-light align-self-start mb-4">
+        <i class="bi bi-arrow-left"></i> Back to activities
+      </button>
+    </router-link>
+    <h1 class="mb-3">Activities report</h1>
 
-    <div v-for="(activity, index) in activities" :key="index">
-      <h2>{{ activity.name }}</h2>
-      <div class="row">
-        <div v-for="(image, i) in activity.images" :key="i" class="col-md-4 mt-4">
-          <div :class="['card', { 'bg-primary': image.marked }]" @click="markImage(activity, i)">
-            <img :src="image.filepath" class="card-img-top" alt="Image" />
-            <div class="card-body">
-              <p class="card-text">{{ image.originalName }}</p>
+    <div v-if="activitiesStore.loading" class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <!-- <div class="row mb-1">
+      <div class="col-4">Activity</div>
+      <div class="col-5">Images</div>
+      <div class="col-3">Selected images</div>
+    </div> -->
+    <div
+      class="row border border-solid border-1 p-3"
+      v-if="!activitiesStore.loading"
+      v-for="(activity, index) in activities"
+      :key="index"
+    >
+      <div class="col-4 border-end">
+        <span class="badge rounded-pill bg-success mb-2">{{ activity.theme }}</span>
+        <h5 class="mb-3">{{ activity.name }}</h5>
+        <p class="m-0">Finished at: {{ formatDate(activity.endDate) }}</p>
+        <p class="m-0">Started at: {{ formatDate(activity.startDate) }}</p>
+        <p class="m-0">
+          Duration in days:
+          {{ getDifferenceInDays(new Date(activity.endDate), new Date(activity.startDate)) }}
+        </p>
+      </div>
+      <div class="col-5 border-end">
+        <div class="row g-3">
+          <div v-for="(image, i) in activity.images" :key="i" class="col-4">
+            <div
+              :class="['p-2 d-flex justify-content-center ', { 'bg-warning': image.marked }]"
+              @click="markImage(activity, i)"
+            >
+              <img :src="image.filepath" class="card-img-top gallery-image" alt="Image" />
             </div>
           </div>
         </div>
       </div>
-      <button class="btn btn-primary mt-4" @click="showMarkedImages(activity)">
-        Show Marked Images
-      </button>
-      <div class="mt-4" v-if="activity.markedImages.length > 0">
-        <h3>Marked Images:</h3>
-        <ul>
-          <li v-for="(name, i) in activity.markedImages" :key="i">{{ name }}</li>
-        </ul>
+      <div class="col-2" v-if="activity.images && activity.images.some((i) => i.marked)">
+        <div v-for="(image, i) in activityMarkedImages(activity)" :key="i">
+          <h4>{{ image.originalName }}</h4>
+        </div>
       </div>
+
+      <!-- <button class="btn btn-primary mt-4" @click="showMarkedImages(activity)">
+          Show Marked Images
+        </button> -->
     </div>
   </div>
 </template>
@@ -30,32 +63,68 @@
 <script>
 import { ref, onMounted } from 'vue'
 import { useActivitiesStore } from '../../stores/activities'
+import { useUserStore } from '../../stores/user'
 
 export default {
   setup() {
     const activitiesStore = useActivitiesStore()
+    const userStore = useUserStore()
     const activities = ref([])
 
     function markImage(activity, index) {
+      console.log('BOO')
       activity.images[index].marked = !activity.images[index].marked
+      console.log(activity.images[index].marked)
     }
 
     function showMarkedImages(activity) {
       activity.markedImages = activity.images
         .filter((image) => image.marked)
-        .map((image) => image.name)
+        .map((image) => image.originalName)
     }
 
-    onMounted(() => {
-      activitiesStore.fetchUserActivities()
-      activities.value = activitiesStore.activities
+    function formatDate(stringDate) {
+      const date = new Date(stringDate)
+      return date.toLocaleString('lt-LT').split(' ')[0]
+    }
+
+    function getDifferenceInDays(date1, date2) {
+      const oneDay = 24 * 60 * 60 * 1000 // Number of milliseconds in a day
+      const timeDiff = Math.abs(date2 - date1) // Get the absolute difference in milliseconds
+      const diffDays = Math.round(timeDiff / oneDay) // Divide by the number of milliseconds in a day and round to the nearest whole number
+      return diffDays
+    }
+
+    const activityMarkedImages = (activity) => activity.images.filter((i) => i.marked)
+
+    onMounted(async () => {
+      await activitiesStore.fetchUserActivities()
+      await userStore.getMe()
+      activities.value = activitiesStore.finished
+      console.log(activities.value)
     })
 
     return {
+      userStore,
+      activitiesStore,
       activities,
       markImage,
-      showMarkedImages
+      showMarkedImages,
+      activityMarkedImages,
+      formatDate,
+      getDifferenceInDays
     }
   }
 }
 </script>
+
+<style>
+.inner-border {
+  box-shadow: inset 0 0 0 2px #000000;
+}
+.gallery-image {
+  width: 140px;
+  height: 140px;
+  object-fit: cover;
+}
+</style>
